969c821f150c22a854a4e2df33dffd7a2980ee0f
Zettex <safaandsafa4@gmail.com>
2025-09-23 21:30:05 +0600

    added the order page

--- app.js
+++ app.js
@@ -1,4 +1,37 @@
-function purr(){
+function purr() {
     let purr = document.getElementById('purr')
     purr.play()
+}
+
+function handleNum() {
+    purr()
+    let amount = document.getElementsByClassName('amount')[0];
+    amount.innerText = parseInt(amount.innerText) + 1;
+    console.log('okay')
+}
+
+function downloadPDF() {
+    purr()
+    const element = document.getElementById("invoice");
+
+    // এখানে html2pdf দিয়ে convert হচ্ছে
+    html2pdf()
+        .from(element)
+        .set({
+            margin: 10,
+            filename: 'invoice.pdf',
+            image: {
+                type: 'jpeg',
+                quality: 0.98
+            },
+            html2canvas: {
+                scale: 2
+            },
+            jsPDF: {
+                unit: 'mm',
+                format: 'a4',
+                orientation: 'portrait'
+            }
+        })
+        .save();
 }
