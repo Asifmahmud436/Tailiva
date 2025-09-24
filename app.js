2540224c1789e5dd2e5e8e02309d0dd86c98bef5
Zettex <safaandsafa4@gmail.com>
2025-09-24 22:01:22 +0600

    completed the download

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 function purr() {
-    let purr = document.getElementById('purr')
-    purr.play()
+    let purr = document.getElementById('purr');
+    purr.play();
 }
 
 function countTotal() {
@@ -43,4 +43,59 @@ function gotoOrder() {
         behavior: "smooth"
     })
 }
+
+function validateForm() {
+    let name = document.getElementById("name").value.trim();
+    let phone = document.getElementById("phone").value.trim();
+    let address = document.getElementById("address").value.trim();
+    let transactionId = document.getElementById("transactionId").value.trim();
+    let location = document.querySelector('input[name="location"]:checked').value;
+    let amount = parseInt(document.getElementsByClassName('amount')[0].innerText);
+    let total = parseInt(document.getElementById('payment-amount').innerText);
+
+    if (!name || !phone || !address || !transactionId || !location) {
+        alert("⚠️ Please fill out all fields before checkout!");
+        return;
+    }
+
+    localStorage.setItem("invoiceData", JSON.stringify({
+        name,
+        phone,
+        address,
+        transactionId,
+        amount,
+        total
+    }));
+    window.location.href = "invoice.html";
+    purr();
+    // console.log(name,phone,address,transactionId,location,amount,total)
+    // let container = document.getElementById('invoice-section');
+    // let div = document.createElement('div');
+    // div.innerHTML = `
+    //     <div class="invoice-header">
+    //       <h1>Tailiva</h1>
+    //       <img src="images/logo.png" alt="" />
+    //     </div>
+    //     <hr />
+    //     <div class="invoice-body">
+    //       <div>
+    //         <h3>${name}</h3>
+    //         <p>Tailiva Permium Chicken (1kg) x ${amount}</p>
+    //         <h1>Tk. ${total}</h1>
+    //       </div>
+    //       <p>Address: <span>${address}</span></p>
+    //       <p>Transaction Id: <span>${transactionId}</span></p>
+    //       <div class="paid-seal">Paid</div>
+    //     </div>
+    //     <div class="invoice-footer">
+    //       <hr />
+    //       <p>The WK IT - 01711738488</p>
+    //     </div>
+    
+    // `;
+    // container.appendChild(div);
+
+    // alert("✅ Order placed successfully! Invoice downloading...");
+}
+
 
